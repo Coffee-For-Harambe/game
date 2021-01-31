f381570eafeacd0b7efe2b49ad6759f856fa7b32
Conan Browne <Ventrik101@Gmail.com>
2021-01-31 11:39:09 -0500

    added base class info

--- src/shared/characterdefinition.js
+++ src/shared/characterdefinition.js
@@ -1,1 +1,18 @@
-export default class CharacterDefinition {}
+export default class CharacterDefinition {
+  constructor(health, damage, movement, range, resistance, initiative, points) {
+    this.name = ""
+    this.health = health
+    this.damage = damage
+    this.movement = movement
+    this.range = range
+    this.resistance = resistance
+    this.initiative = initiative
+    this.points = points
+  }
+  attack() {
+    return this.damage
+  }
+  receiveDamage(damage) {
+    this.health -= damage
+  }
+}
