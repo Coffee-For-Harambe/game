4c4dda5c16fe0c18d8d6c7393d3552c262d3c1b4
Xavier Bergeron <xavbergeron@gmail.com>
2021-02-02 15:59:16 -0800

    Update sandbox file for Conan to play with

--- src/sandbox.js
+++ src/sandbox.js
@@ -2,4 +2,91 @@ import "./shared/index"
 import Game from "./shared/game"
 const game = new Game()
 
-console.log("Game is woo", game)
+import { printGrid } from "./shared/gridutils"
+
+const human = game.teams[0]
+const computer = game.teams[1]
+
+function showGrid(grid, hide = "00") {
+  let gridStr = printGrid(grid)
+  gridStr = gridStr.replaceAll(hide, "--")
+  console.log(gridStr)
+}
+
+function show() {
+  showGrid(game.getCharacterGrid(), "01")
+}
+
+function clamp(i) {
+  if (i < 0) return 0
+  if (i > 15) return 15
+  return i
+}
+
+function randomCharacter(team) {
+  return team.characters[Math.floor(Math.random() * team.characters.length)]
+}
+
+function moveRandomCharacter(team) {
+  const character = randomCharacter(team)
+
+  const randX =
+    Math.floor(Math.random() * character.movement * 2) - character.movement
+  const randY =
+    Math.floor(Math.random() * character.movement * 2) - character.movement
+
+  character.moveSprite(clamp(character.x + randX), clamp(character.y + randY))
+}
+
+/*  ***** USEFUL *****
+
+// Prints the board to the console
+show() // OR showGrid(game.characterGrid)
+
+// Print the influence grids
+showGrid(human.influenceGrid)
+showGrid(computer.influenceGrid)
+
+// Print the team's characters grid
+showGrid(human.teamGrid)
+showGrid(computer.teamGrid)
+
+// Update the influence, team and character grids at the end of each move
+game.advanceTurn()  // <- THIS IS REQUIRED!!!
+
+// Returns a string of the grid without hiding the zeroes or ones
+console.log(printGrid(someGrid))
+
+// Get a random cahracter from a team
+const char = randomCharacter(team)
+
+// Randomly move a cahracter from that team
+moveRandomCharacter(team)
+game.advanceTurn()
+
+
+// Remember to select debug config to "Watch Sandbox" and you can use
+// Debugger breakpoints to pause the code and show
+
+*/
+
+/****************************************************************
+ * BE CAREFUL NOT TO EDIT ABOVE THIS LINE UNLESS YOU ABSOLUTELY NEED TO
+ */
+
+show()
+
+moveRandomCharacter(human)
+game.advanceTurn()
+
+show()
+
+moveRandomCharacter(computer)
+game.advanceTurn()
+
+show()
+
+moveRandomCharacter(computer)
+game.advanceTurn()
+
+show()
