48bd61acf406bf4247a8b09069b2f59692588848
John Vasile <johny_tnt@yahoo.com>
2021-02-02 22:51:13 -0500

    Spreads Characters on the board

--- src/shared/game.js
+++ src/shared/game.js
@@ -14,19 +14,20 @@ export default class Game {
     this.teams = []
     this.teams[0] = new Team([
       new Character.Rogue(1, 15),
-      new Character.Monk(2, 15),
-      new Character.Ranger(3, 15),
-      new Character.Warrior(4, 15),
-      new Character.Wizard(5, 15),
+      new Character.Monk(4, 15),
+      new Character.Ranger(7, 15),
+      new Character.Warrior(10, 15),
+      new Character.Wizard(13, 15),
     ])
 
     this.teams[1] = new Team([
       new Character.Skeleton(0, 0),
-      new Character.Skeleton(1, 0),
-      new Character.Skeleton(2, 0),
-      new Character.Dragon(3, 0),
-      new Character.Skeleton(4, 0),
-      new Character.Skeleton(5, 0),
+      new Character.Skeleton(3, 0),
+      new Character.Skeleton(7, 0),
+      new Character.Dragon(9, 0),
+
+      new Character.Skeleton(13, 0),
+      new Character.Skeleton(15, 0),
     ])
 
     this.teams.forEach((team) => {
