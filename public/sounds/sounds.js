9db9ac58fe24a6a5f9fbb66e7a2c8b2ca4f59250
Conan Browne <Ventrik101@Gmail.com>
2021-02-04 17:02:27 -0500

    Fixed audio file names, added sound.js

---
+++ public/sounds/sounds.js
@@ -0,0 +1,133 @@
+/****************************************************************
+ * M U S I C ****************************************************
+ ****************************************************************/
+"songBattle1" : {
+  url : "./music/shadowsOfMisdeeds_battleA.mp3"
+},
+
+"songBattle2" : {
+  url : "./music/aFightInFields_battleB.mp3"
+},
+
+"songTitle" : {
+  url : "./music/mushroomDance_title.ogg"
+},
+
+"songWin" : {
+  url : "./music/cantStopWinning_win.mp3"
+},
+
+"songLose" : {
+  url : "./music/theFallen_lose.mp3"
+},
+
+"songCredits" : {
+  url : "./music/visionsOfFreedom_credits.mp3"
+},
+
+/****************************************************************
+ * S F X ********************************************************
+ ****************************************************************/
+
+/* Footsteps */
+"foot0" : {
+  url : "./sfx/footsteps/Footstep_Dirt_00.mp3"
+},
+
+"foot1" : {
+  url : "./sfx/footsteps/Footstep_Dirt_01.mp3"
+},
+
+"foot2" : {
+  url : "./sfx/footsteps/Footstep_Dirt_02.mp3"
+},
+
+"foot3" : {
+  url : "./sfx/footsteps/Footstep_Dirt_03.mp3"
+},
+
+"foot4" : {
+  url : "./sfx/footsteps/Footstep_Dirt_04.mp3"
+},
+
+/* Hits */
+
+"hit0" : {
+  url : "./sfx/hits/hit0.flac"
+},
+
+"hit1" : {
+  url : "./sfx/hits/hit1.flac"
+},
+
+"hit2" : {
+  url : "./sfx/hits/hit2.flac"
+},
+
+//** MONSTERS **//
+
+/* Dragon */
+"flap" : {
+  url : "./sfx/monsters/dragonflap.mp3"
+},
+
+"dGrowl0" : {
+  url : "./sfx/monsters/Dragon_Growl_00.mp3"
+},
+
+"dGrowl1" : {
+  url : "./sfx/monsters/Dragon_Growl_01.mp3"
+},
+
+/* Skelly boi */
+"sGroan0" : {
+  url : "./sfx/monsters/skelly0.wav"
+},
+
+"sGroan1" : {
+  url : "./sfx/monsters/skelly1.wav"
+},
+
+"sGroan2" : {
+  url : "./sfx/monsters/skelly2.wav"
+},
+
+
+/* Spells */
+"zap0" : {
+  url : "./sfx/spell/zap0.flac"
+},
+
+"zap1" : {
+  url : "./sfx/spell/zap1.flac"
+},
+
+/* Swishy swooshy */
+"swish0" : {
+  url : "./sfx/swish/swish0.wav"
+},
+
+"swish1" : {
+  url : "./sfx/swish/swish1.wav"
+},
+
+"swish2" : {
+  url : "./sfx/swish/swish2.wav"
+},
+
+"swish3" : {
+  url : "./sfx/swish/swish3.wav"
+},
+
+/* Swishy swooshy */
+"sword0" : {
+  url : "./sfx/sword/sword0.wav"
+},
+
+"sword0" : {
+  url : "./sfx/sword/sword1.wav"
+},
+
+"sword0" : {
+  url : "./sfx/sword/sword2.wav"
+}
