22c4eefb934f25cf5e2bc700e9f1e02ca0e50378
Conan Browne <Ventrik101@Gmail.com>
2021-02-04 15:42:19 -0500

    Brains...

--- src/shared/brain/zerosum.js
+++ src/shared/brain/zerosum.js
@@ -6,68 +6,25 @@
 
 // THIS SHIT HERE MOVES THE NPC AROUND.
 function npcMove(char) {
-  // Check current position influence.
-  const x = char.x
-  const y = char.y
-  const currentInfluence = human.influenceGrid[y][x]
-  // Empty Strings for determining best possible move.
-  let highX = 0
-  let highY = 0
-  let highW = 0
-  // let highW = human.influenceGrid[highY][highX]
-  // let highW = currentInfluence
-  let curPC = 0
-  // Get the index value for each character in the grid.
-  // for (let i = 0; i < human.characters.length; i++) {
-  //   curPC = i
-  //   console.log(
-  //     "X =",
-  //     human.characters[curPC].x,
-  //     "Y =",
-  //     human.characters[curPC].y
-  //   )
-  // }
-
-  // for (let n = 0; n < human.characters.length; n++) {
-  //   curPC = n
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-      let d = distanceTo({ x: highX, y: highY }, { x: i, y: j })
-      highX = human.characters[curPC].x
-      highY = human.characters[curPC].y
-      highW = human.influenceGrid[highY][highX - 1]
-      // console.log("High X/Y X =", highX, "Y =", highY)
-      // console.log(d)
-      console.log(highW)
-      // if (d < char.movement && currentInfluence > highW) {
-      //   highX = human.characters[curPC].x
-      //   highY = human.characters[curPC].y
-      //   highW = human.influenceGrid[highX][highY]
-      // }
+  let cx = char.x,
+    cy = char.y
+  let goodChoice = []
+  let bestChoice = { x: cx, y: cy, influence: human.influenceGrid[cy][cx] }
+  for (let y = 0; y < 16; y++) {
+    for (let x = 0; x < 16; x++) {
+      const weight = human.influenceGrid[y][x]
+      let dist = distanceTo({ x: cx, y: cy }, { x: x, y: y })
+      if (dist > char.movement) {
+        dist *= 1.5
+      }
+      const realInfluence = weight - dist
+      if (realInfluence > bestChoice.influence) {
+        goodChoice.push(bestChoice) // No longer the best but we'll keep you in mind
+        bestChoice = { x: x, y: y, influence: realInfluence }
+      }
+      /* check position vs best choice */
     }
+    console.log(char.shortCode, "x=", cx, "y=", cy)
+    console.log("Our best choice so far seems to be", bestChoice)
   }
-  /*if distance is equal too or less than char movement distance
-    then move to new position
-    else try and move character */
-  // }
-
-  console.log(char)
-  console.log("High X/Y X =", highX, "Y =", highY)
-  console.log(
-    human.characters[curPC].name,
-    "X =",
-    human.characters[curPC].x,
-    "Y =",
-    human.characters[curPC].y
-  )
-  // Find where the PC is on the grid via influence
-  // Move towards the highest influence point within the alloted movement distance
 }
-
-// function npcAtk(char) {
-//   // We should check for attack before performing a move
-//   // Just perform a check to make see if the NPC is standing next to a PC,
-//   // This check needs to compare attack range to distance from player.
-//   // IF YES then perform attack at their grid pos.
-//   // else goto move function (eval)
-// }
