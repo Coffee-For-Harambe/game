8574c8a2562ff958a66c30d77212260747f07335
Xavier Bergeron <xavbergeron@gmail.com>
2021-02-04 17:49:19 -0800

    Fix sound paths

--- src/client/sounds/index.js
+++ src/client/sounds/index.js
@@ -9,12 +9,12 @@ export const songWin = new Audio()
 export const songLose = new Audio()
 export const songCredits = new Audio()
 
-songBattle0.src = "/music/shadowsOfMisdeeds_battleA.mp3"
-songBattle1.src = "/music/aFightInFields_battleB.mp3"
-songTitle.src = "/music/mushroomDance_title.ogg"
-songWin.src = "/music/cantStopWinning_win.mp3"
-songLose.src = "/music/theFallen_lose.mp3"
-songCredits.src = "/music/visionsOfFreedom_credits.mp3"
+songBattle0.src = "/sounds/music/shadowsOfMisdeeds_battleA.mp3"
+songBattle1.src = "/sounds/music/aFightInFields_battleB.mp3"
+songTitle.src = "/sounds/music/mushroomDance_title.ogg"
+songWin.src = "/sounds/music/cantStopWinning_win.mp3"
+songLose.src = "/sounds/music/theFallen_lose.mp3"
+songCredits.src = "/sounds/music/visionsOfFreedom_credits.mp3"
 
 /****************************************************************
  * S F X ********************************************************
@@ -28,11 +28,11 @@ export const foot2 = new Audio()
 export const foot3 = new Audio()
 export const foot4 = new Audio()
 
-foot0.src = "/sfx/footsteps/Footstep_Dirt_00.mp3"
-foot1.src = "/sfx/footsteps/Footstep_Dirt_01.mp3"
-foot2.src = "/sfx/footsteps/Footstep_Dirt_02.mp3"
-foot3.src = "/sfx/footsteps/Footstep_Dirt_03.mp3"
-foot4.src = "/sfx/footsteps/Footstep_Dirt_04.mp3"
+foot0.src = "/sounds/sfx/footsteps/Footstep_Dirt_00.mp3"
+foot1.src = "/sounds/sfx/footsteps/Footstep_Dirt_01.mp3"
+foot2.src = "/sounds/sfx/footsteps/Footstep_Dirt_02.mp3"
+foot3.src = "/sounds/sfx/footsteps/Footstep_Dirt_03.mp3"
+foot4.src = "/sounds/sfx/footsteps/Footstep_Dirt_04.mp3"
 
 /* Hits */
 
@@ -40,9 +40,9 @@ export const hit0 = new Audio()
 export const hit1 = new Audio()
 export const hit2 = new Audio()
 
-hit0.src = "/sfx/hits/hit0.flac"
-hit1.src = "/sfx/hits/hit2.flac"
-hit2.src = "/sfx/hits/hit1.flac"
+hit0.src = "/sounds/sfx/hits/hit0.flac"
+hit1.src = "/sounds/sfx/hits/hit2.flac"
+hit2.src = "/sounds/sfx/hits/hit1.flac"
 
 //** MONSTERS **//
 
@@ -52,9 +52,9 @@ export const flap = new Audio()
 export const dGrowl0 = new Audio()
 export const dGrowl1 = new Audio()
 
-flap.src = "/sfx/monsters/dragonflap.mp3"
-dGrowl0.src = "/sfx/monsters/Dragon_Growl_00.mp3"
-dGrowl1.src = "/sfx/monsters/Dragon_Growl_01.mp3"
+flap.src = "/sounds/sfx/monsters/dragonflap.mp3"
+dGrowl0.src = "/sounds/sfx/monsters/Dragon_Growl_00.mp3"
+dGrowl1.src = "/sounds/sfx/monsters/Dragon_Growl_01.mp3"
 
 /* Skelly boi */
 
@@ -62,17 +62,17 @@ export const sGroan0 = new Audio()
 export const sGroan1 = new Audio()
 export const sGroan2 = new Audio()
 
-sGroan0.src = "/sfx/monsters/skelly0.wav"
-sGroan1.src = "/sfx/monsters/skelly1.wav"
-sGroan2.src = "/sfx/monsters/skelly2.wav"
+sGroan0.src = "/sounds/sfx/monsters/skelly0.wav"
+sGroan1.src = "/sounds/sfx/monsters/skelly1.wav"
+sGroan2.src = "/sounds/sfx/monsters/skelly2.wav"
 
 /* Spells */
 
 export const zap0 = new Audio()
 export const zap1 = new Audio()
 
-zap0.src = "/sfx/spell/zap0.flac"
-zap1.src = "/sfx/spell/zap1.flac"
+zap0.src = "/sounds/sfx/spell/zap0.flac"
+zap1.src = "/sounds/sfx/spell/zap1.flac"
 
 /* Swishy swooshy */
 
@@ -81,10 +81,10 @@ export const swish1 = new Audio()
 export const swish2 = new Audio()
 export const swish3 = new Audio()
 
-swish0.src = "/sfx/swish/swish0.wav"
-swish1.src = "/sfx/swish/swish1.wav"
-swish2.src = "/sfx/swish/swish2.wav"
-swish3.src = "/sfx/swish/swish3.wav"
+swish0.src = "/sounds/sfx/swish/swish0.wav"
+swish1.src = "/sounds/sfx/swish/swish1.wav"
+swish2.src = "/sounds/sfx/swish/swish2.wav"
+swish3.src = "/sounds/sfx/swish/swish3.wav"
 
 /* Sword */
 
@@ -92,6 +92,6 @@ export const sword0 = new Audio()
 export const sword1 = new Audio()
 export const sword2 = new Audio()
 
-sword0.src = "/sfx/sword/sword0.wav"
-sword1.src = "/sfx/sword/sword1.wav"
-sword2.src = "/sfx/sword/sword2.wav"
+sword0.src = "/sounds/sfx/sword/sword0.wav"
+sword1.src = "/sounds/sfx/sword/sword1.wav"
+sword2.src = "/sounds/sfx/sword/sword2.wav"
