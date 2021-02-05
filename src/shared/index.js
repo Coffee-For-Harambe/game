92aa9465c2b35acd1e85a3807b96d21e5e862b3c
James Post <avilapost93@gmail.com>
2021-02-05 11:06:59 -0800

    changes to debug visibility

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,3 +1,20 @@
 import "./game"
 import "./logic"
 import "./brain/zerosum"
+
+document.getElementById("debug").style.visibility = "hidden"
+document.getElementById("debugjson").style.visibility = "hidden"
+
+document.addEventListener("keydown", function (e) {
+  if (e.code == "KeyZ") {
+    document.getElementById("debug").style.visibility = "visible"
+    document.getElementById("debugjson").style.visibility = "visible"
+  }
+})
+
+document.addEventListener("keydown", function (e) {
+  if (e.code == "KeyX") {
+    document.getElementById("debug").style.visibility = "hidden"
+    document.getElementById("debugjson").style.visibility = "hidden"
+  }
+})
