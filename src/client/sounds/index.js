75a13469d24e041caf807136a112f7fd8cf2baab
Conan Browne <Ventrik101@Gmail.com>
2021-02-04 20:00:38 -0500

    gdi lol don't look

---
+++ src/client/sounds/index.js
@@ -0,0 +1,97 @@
+/****************************************************************
+ * M U S I C ****************************************************
+ ****************************************************************/
+
+let songBattle0 = new Audio()
+let songBattle1 = new Audio()
+let songTitle = new Audio()
+let songWin = new Audio()
+let songLose = new Audio()
+let songCredits = new Audio()
+
+songBattle0.src = "./music/shadowsOfMisdeeds_battleA.mp3"
+songBattle1.src = "./music/aFightInFields_battleB.mp3"
+songTitle.src = "./music/mushroomDance_title.ogg"
+songwin.src = "./music/cantStopWinning_win.mp3"
+songLose.src = "./music/theFallen_lose.mp3"
+songCredits.src = "./music/visionsOfFreedom_credits.mp3"
+
+/****************************************************************
+ * S F X ********************************************************
+ ****************************************************************/
+
+/* Footsteps */
+
+let foot0 = new Audio()
+let foot1 = new Audio()
+let foot2 = new Audio()
+let foot3 = new Audio()
+let foot4 = new Audio()
+
+foot0.src = "./sfx/footsteps/Footstep_Dirt_00.mp3"
+foot1.src = "./sfx/footsteps/Footstep_Dirt_01.mp3"
+foot2.src = "./sfx/footsteps/Footstep_Dirt_02.mp3"
+foot3.src = "./sfx/footsteps/Footstep_Dirt_03.mp3"
+foot4.src = "./sfx/footsteps/Footstep_Dirt_04.mp3"
+
+/* Hits */
+
+let hit0 = new Audio()
+let hit1 = new Audio()
+let hit2 = new Audio()
+
+hit0.src = "./sfx/hits/hit0.flac"
+hit1.src = "./sfx/hits/hit2.flac"
+hit2.src = "./sfx/hits/hit1.flac"
+
+//** MONSTERS **//
+
+/* Dragon */
+
+let flap = new Audio()
+let dGrowl0 = new Audio()
+let dGrowl1 = new Audio()
+
+flap, (src = "./sfx/monsters/dragonflap.mp3")
+dGrowl0.src = "./sfx/monsters/Dragon_Growl_00.mp3"
+dGrowl1.src = "./sfx/monsters/Dragon_Growl_01.mp3"
+
+/* Skelly boi */
+
+let sGroan0 = new Audio()
+let sGroan1 = new Audio()
+let sGroan2 = new Audio()
+
+sGroan0.src = "./sfx/monsters/skelly0.wav"
+sGroan1.src = "./sfx/monsters/skelly1.wav"
+sGroan2.src = "./sfx/monsters/skelly2.wav"
+
+/* Spells */
+
+let zap0 = new Audio()
+let zap1 = new Audio()
+
+zap0.src = "./sfx/spell/zap0.flac"
+zap1.src = "./sfx/spell/zap1.flac"
+
+/* Swishy swooshy */
+
+let swish0 = new Audio()
+let swish1 = new Audio()
+let swish2 = new Audio()
+let swish3 = new Audio()
+
+swish0.src = "./sfx/swish/swish0.wav"
+swish1.src = "./sfx/swish/swish1.wav"
+swish2.src = "./sfx/swish/swish2.wav"
+swish3.src = "./sfx/swish/swish3.wav"
+
+/* Sword */
+
+let sword0 = new Audio()
+let sword1 = new Audio()
+let sword2 = new Audio()
+
+sword0.src = "./sfx/sword/sword0.wav"
+sword1.src = "./sfx/sword/sword1.wav"
+sword2.src = "./sfx/sword/sword2.wav"
