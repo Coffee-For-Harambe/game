3dc404de5e5c431a023af2158183566e7d890856
John Vasile <johny_tnt@yahoo.com>
2021-02-08 01:42:30 -0500

    Credits Page

--- src/client/ui/startmenu.js
+++ src/client/ui/startmenu.js
@@ -4,6 +4,13 @@ function hideElement() {
   // document.querySelector("#ui").toggleClass("ui.hide")
 }
 
+function showCredits() {
+  hideElement()
+  let creditsG = document.getElementById("credits")
+  element.classList.toggle("show")
+  // document.querySelector("#ui").toggleClass("ui.hide")
+}
+
 document.querySelector("#start").addEventListener("click", hideElement)
 document.querySelector("#new").addEventListener("click", hideElement)
-document.querySelector("#Options").addEventListener("click", hideElement)
+document.querySelector("#credits").addEventListener("click", showCredits)
