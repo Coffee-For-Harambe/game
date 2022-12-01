23428424040c23df34b1a885abc87f9e940ad667
john-vasile <john.Vs11@hotmail.com>
2022-12-01 11:56:25 -0500

    adds start button

--- src/client/ui/startmenu.js
+++ src/client/ui/startmenu.js
@@ -1,16 +1,42 @@
-function hideElement() {
-  let element = document.getElementById("ui")
-  element.classList.toggle("hide")
-  // document.querySelector("#ui").toggleClass("ui.hide")
+let startBtn = document.getElementById('start_btn');
+startBtn.addEventListener("click", _ =>{
+  console.log('working')
+  toggleScreen('start_menu', false);
+  toggleScreen('canvas', true);
+
+})
+
+
+function toggleScreen(id,toggle) {
+  let element = document.getElementById(id);
+  let display = ( toggle ) ? 'block' : 'none';
+  element.style.display = display;
 }
 
-function showCredits() {
-  hideElement()
-  let creditsG = document.getElementById("credits")
-  element.classList.toggle("show")
-  // document.querySelector("#ui").toggleClass("ui.hide")
-}
 
-document.querySelector("#start").addEventListener("click", hideElement)
-document.querySelector("#new").addEventListener("click", hideElement)
-document.querySelector("#credits").addEventListener("click", showCredits)
+
+
+
+
+
+
+
+
+
+
+// function hideElement() {
+//   let element = document.getElementById("ui")
+//   element.classList.toggle("hide")
+//   // document.querySelector("#ui").toggleClass("ui.hide")
+// }
+
+// function showCredits() {
+//   hideElement()
+//   let creditsG = document.getElementById("credits")
+//   element.classList.toggle("show")
+//   // document.querySelector("#ui").toggleClass("ui.hide")
+// }
+
+// document.querySelector("#start").addEventListener("click", hideElement)
+// document.querySelector("#new").addEventListener("click", hideElement)
+// document.querySelector("#credits").addEventListener("click", showCredits)
